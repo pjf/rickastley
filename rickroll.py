075c8025ca14859810da9c4ed558b991d812e79a
Paul Fenwick <pjf@perltraining.com.au>
2016-11-19 18:39:38 +1300

    Proof of concept flask app

---
+++ rickroll.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from flask import Flask
+app = Flask(__name__)
+
+from twilio import twiml
+
+orig_song = "https://dl.dropboxusercontent.com/u/9702672/music/01-NeverGonnaGiveYouUp.mp3"
+menu     = "Menu disabled in development."
+
+@app.route("/")
+def original():
+    response = twiml.Response()
+
+    gather = response.gather(numDigits=1, timeout=10)
+    gather.play(orig_song)
+
+    response.say(menu)
+
+    return str(response)
+
+if __name__ == "__main__":
+    app.run()
